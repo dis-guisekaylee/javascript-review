ec0a98a6bdbd035ce1df980b17f0f308a66b33d0
Kaylee Guise <guisekayl@gmail.com>
2022-08-17 16:47:10 -0500

    Started reviewing course material, initialize conditionals lab

--- day1/practice-day1.js
+++ day1/practice-day1.js
@@ -1,1 +1,19 @@
-let name = "kaylee";
+let developerName = "kaylee guise";
+
+developerName = "shrek";
+
+let devFirstName = "Kaylee";
+let devSurname = "Guise";
+let devLocation = "Milwaukee, WI"
+let techStack = "MERN";
+let expandedStack = [`MongoDB, Express, React.js, Node.js`];
+
+console.log(devFirstName + " " + devSurname + " is a full stack developer located in " + devLocation + " and creates web applications with the " + techStack + ". " + techStack + " stands for " + expandedStack + ".");
+
+devFirstName = "Billie Joe";
+devSurname = "Armstrong";
+devLocation = "Oakland, CA";
+techStack = "Green Day";
+expandedStack = [`American Idiot, Dookie, Nimrod, 21st Century Breakdown`];
+
+console.log(devFirstName + " " + devSurname + " is a musician located in " + devLocation + " and creates rock music with " + techStack + ". " + techStack + " has written some noteworthy albums, including " + expandedStack + ".");
